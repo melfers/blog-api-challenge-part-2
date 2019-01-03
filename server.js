118ecd8851bc726b03f01a84c3567036155f78d5
Molly Elfers <elfers.molly@gmail.com>
2019-01-03 13:16:50 -0800

    added GET requests and models/schemas

--- server.js
+++ server.js
@@ -13,6 +13,59 @@ mongoose.Promise = global.Promise;
 app.use(morgan("common"));
 app.use(express.json());
 
+// GET requests to /restaurants => return 10 restaurants
+app.get("/blog-posts", (req, res) => {
+  Post.find()
+    .limit(10)
+    // success callback: for each restaurant we got back, we'll
+    // call the `.serialize` instance method we've created in
+    // models.js in order to only expose the data we want the API return.    
+    .then(posts => {
+      res.json({
+        posts: posts.map(post => post.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.get("/blog-posts/:id", (req, res) => {
+  Post
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.post("/blog-posts", (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
 let server;
 
 function runServer(databaseUrl, port = PORT) {
